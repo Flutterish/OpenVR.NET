04d22d026ce2126891dc630b25e8139029bf3a86
Flutterish <CharmindYT@gmail.com>
2022-05-20 23:14:15 +0200

    colored vertices

--- VisualTests/TestWindow.cs
+++ VisualTests/TestWindow.cs
@@ -9,11 +9,13 @@ internal class TestWindow : GameWindow {
 		: base( gameWindowSettings, nativeWindowSettings ) {
 
 		basicShader = new( "Resources/Shaders/basic.vert", "Resources/Shaders/basic.frag" );
+		colorShader = new( "Resources/Shaders/colored.vert", "Resources/Shaders/colored.frag" );
 	}
 
 	Shader basicShader;
+	Shader colorShader;
 
-	Vector3[] shapeData = null!;
+	ColoredVertex[] shapeData = null!;
 	uint[] shapeIndices = null!;
 	GlHandle VAO;
 	GlHandle VBO;
@@ -21,11 +23,11 @@ internal class TestWindow : GameWindow {
 	protected override void OnLoad () {
 		base.OnLoad();
 
-		shapeData = new Vector3[] {
-			new(  0.5f,  0.5f, 0.0f ),
-			new(  0.5f, -0.5f, 0.0f ),
-			new( -0.5f, -0.5f, 0.0f ),
-			new( -0.5f,  0.5f, 0.0f )
+		shapeData = new ColoredVertex[] {
+			new() { Position = new(  0.5f,  0.5f, 0.0f ), Color = new( 1, 0, 0 ) },
+			new() { Position = new(  0.5f, -0.5f, 0.0f ), Color = new( 0, 1, 0 ) },
+			new() { Position = new( -0.5f, -0.5f, 0.0f ), Color = new( 0, 0, 1 ) },
+			new() { Position = new( -0.5f,  0.5f, 0.0f ), Color = new( 1, 0, 1 ) }
 		};
 		shapeIndices = new uint[] {
 			0, 1, 3,
@@ -37,8 +39,8 @@ internal class TestWindow : GameWindow {
 
 		VBO = GL.GenBuffer();
 		GL.BindBuffer( BufferTarget.ArrayBuffer, VBO );
-		PositionVertex.Upload( shapeData, shapeData.Length );
-		PositionVertex.Link( position: basicShader.GetAttrib( "aPos" ) );
+		ColoredVertex.Upload( shapeData, shapeData.Length );
+		ColoredVertex.Link( position: colorShader.GetAttrib( "aPos" ), color: colorShader.GetAttrib( "aColor" ) );
 
 		EBO = GL.GenBuffer();
 		GL.BindBuffer( BufferTarget.ElementArrayBuffer, EBO );
@@ -55,8 +57,7 @@ internal class TestWindow : GameWindow {
 		GL.ClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
 		GL.Clear( ClearBufferMask.ColorBufferBit );
 
-		basicShader.Bind();
-		basicShader.SetUniform( "color", new Color4( 0, MathF.Sin(time) / 2 + 0.5f, 0, 1 ) );
+		colorShader.Bind();
 		GL.BindVertexArray( VAO );
 		GL.DrawElements( PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, IntPtr.Zero );
 
