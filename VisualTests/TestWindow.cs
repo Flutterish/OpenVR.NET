6a81a680f1860b3d88a41f07284618c7d10e9a06
Flutterish <CharmindYT@gmail.com>
2022-05-21 00:29:51 +0200

    become one with the cube

--- VisualTests/TestWindow.cs
+++ VisualTests/TestWindow.cs
@@ -33,14 +33,42 @@ internal class TestWindow : GameWindow {
 		base.OnLoad();
 
 		shapeData = new TexturedVertex[] {
-			new() { Position = new(  0.5f,  0.5f, 0.0f ), UV = new( 1, 1 ) },
-			new() { Position = new(  0.5f, -0.5f, 0.0f ), UV = new( 1, 0 ) },
-			new() { Position = new( -0.5f, -0.5f, 0.0f ), UV = new( 0, 0 ) },
-			new() { Position = new( -0.5f,  0.5f, 0.0f ), UV = new( 0, 1 ) }
-		};
-		shapeIndices = new uint[] {
-			0, 1, 3,
-			1, 2, 3
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new(  0.5f, -0.5f, -0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new(  0.5f,  0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new( -0.5f,  0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new( -0.5f, -0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new(  0.5f, -0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new( -0.5f,  0.5f,  0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new( -0.5f, -0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new( -0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new( -0.5f,  0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new( -0.5f, -0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new( -0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new(  0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new(  0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new(  0.5f, -0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new(  0.5f, -0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new(  0.5f, -0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f, -0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new( -0.5f, -0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new( -0.5f, -0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new( -0.5f,  0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) },
+			new() { Position = new(  0.5f,  0.5f, -0.5f ), UV = new( 1.0f, 1.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new(  0.5f,  0.5f,  0.5f ), UV = new( 1.0f, 0.0f ) },
+			new() { Position = new( -0.5f,  0.5f,  0.5f ), UV = new( 0.0f, 0.0f ) },
+			new() { Position = new( -0.5f,  0.5f, -0.5f ), UV = new( 0.0f, 1.0f ) }
 		};
 
 		VAO = GL.GenVertexArray();
@@ -51,9 +79,9 @@ internal class TestWindow : GameWindow {
 		TexturedVertex.Upload( shapeData, shapeData.Length );
 		TexturedVertex.Link( position: unlitShader.GetAttrib( "aPos" ), uv: unlitShader.GetAttrib( "aUv" ) );
 
-		EBO = GL.GenBuffer();
-		GL.BindBuffer( BufferTarget.ElementArrayBuffer, EBO );
-		Indices.Upload( shapeIndices, shapeIndices.Length );
+		//EBO = GL.GenBuffer();
+		//GL.BindBuffer( BufferTarget.ElementArrayBuffer, EBO );
+		//Indices.Upload( shapeIndices, shapeIndices.Length );
 	}
 
 	float time;
@@ -64,11 +92,12 @@ internal class TestWindow : GameWindow {
 
 		GL.Viewport( 0, 0, Size.X, Size.Y );
 		GL.ClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
-		GL.Clear( ClearBufferMask.ColorBufferBit );
+		GL.Enable( EnableCap.DepthTest );
+		GL.Clear( ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit );
 
 		unlitShader.Bind();
 		susie.Bind();
-		unlitShader.SetUniform( "transform", shapeTransform.Matrix );
+		//unlitShader.SetUniform( "transform", shapeTransform.Matrix );
 		unlitShader.SetUniform( "gProj", Matrix4.CreateScale( 1, 1, -1 ) * Matrix4.CreatePerspectiveFieldOfView(
 			MathF.PI / 2,
 			(float)Size.X / Size.Y,
@@ -76,7 +105,25 @@ internal class TestWindow : GameWindow {
 			1000f
 		) );
 		GL.BindVertexArray( VAO );
-		GL.DrawElements( PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, IntPtr.Zero );
+
+		var cubePositions = new Vector3[] {
+			new( 2.0f,  5.0f, 15.0f),
+			new(-1.5f, -2.2f, 2.5f),
+			new(-3.8f, -2.0f, 12.3f),
+			new( 2.4f, -0.4f, 3.5f),
+			new(-1.7f,  3.0f, 7.5f),
+			new( 1.3f, -2.0f, 2.5f),
+			new( 1.5f,  2.0f, 2.5f),
+			new( 1.5f,  0.2f, 1.5f),
+			new(-1.3f,  1.0f, 1.5f)
+		};
+		foreach ( var pos in cubePositions ) {
+			shapeTransform.Position = pos;
+			unlitShader.SetUniform( "transform", shapeTransform.Matrix );
+			GL.DrawArrays( PrimitiveType.Triangles, 0, 36 );
+		}
+		//GL.DrawArrays( PrimitiveType.Triangles, 0, 36 );
+		//GL.DrawElements( PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, IntPtr.Zero );
 
 		SwapBuffers();
 	}
@@ -84,7 +131,7 @@ internal class TestWindow : GameWindow {
 	protected override void OnUpdateFrame ( FrameEventArgs args ) {
 		base.OnUpdateFrame( args );
 
-		shapeTransform.Position = new( 0, 0, 1 );
+		shapeTransform.Position = new( 0, 0, 1.6f );
 		shapeTransform.Rotation *= Quaternion.FromAxisAngle( new Vector3( 1, 0.3f, MathF.Sin( (float)args.Time ) + 1 ).Normalized(), (float)args.Time );
 	}
 }
